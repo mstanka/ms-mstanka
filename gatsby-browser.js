fc02219fa13da9fb24770835f7496047692c4480
mstanka <mice78.mm@gmail.com>
2020-10-03 12:51:03 +0200

    removed unneeded files

--- gatsby-browser.js
+++
@@ -1,9 +0,0 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/browser-apis/
- */
-
-exports.onRouteUpdate = ({ location, action }) => {
-  console.log("route changed", { location, action })
-}
